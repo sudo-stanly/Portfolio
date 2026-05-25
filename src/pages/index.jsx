e906c3321d6b0d6da263c2eae76e0d15402ae51c
sudo-stanly <alys.git24@gmail.com>
2026-05-25 17:19:16 +0800

    making parallax effect

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,26 +4,43 @@ export default function IndexPage(){
     
         
 
-        <main>
-            
-            <header>
-                header
-            </header>
+       
 
-        
-            <section className="section1">
-                section 1
-            </section>
+        <div className="img1">
+            <div className="heading">Fixed</div>
+        </div>
 
+        <section>
+            Fixed Background Website
 
-            <footer>
-                footer
-            </footer>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus tempora nihil mollitia ex dolores maiores culpa excepturi quam laborum ipsam, facilis iure tempore dicta repellat obcaecati quaerat. Fugit, quas recusandae?
+            </p>
+        </section>
 
-        </main>
+        <div className="img2">
+            <div className="heading">Fixed</div>
+        </div>
 
-        
+        <section>
+            Fixed Background Website
 
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus tempora nihil mollitia ex dolores maiores culpa excepturi quam laborum ipsam, facilis iure tempore dicta repellat obcaecati quaerat. Fugit, quas recusandae?
+            </p>
+        </section>
+
+        <div className="img3">
+            <div className="heading">Fixed</div>
+        </div>
+
+        <section>
+            Fixed Background Website
+
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus tempora nihil mollitia ex dolores maiores culpa excepturi quam laborum ipsam, facilis iure tempore dicta repellat obcaecati quaerat. Fugit, quas recusandae?
+            </p>
+        </section>
        
 
     </>);
