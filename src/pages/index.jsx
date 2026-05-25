3fc5d9ed8ead594749d1c5ebe8027b4245406f81
sudo-stanly <alys.git24@gmail.com>
2026-05-25 17:28:14 +0800

    fixed background

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -33,14 +33,6 @@ export default function IndexPage(){
         <div className="img3">
             <div className="heading">Fixed</div>
         </div>
-
-        <section>
-            Fixed Background Website
-
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus tempora nihil mollitia ex dolores maiores culpa excepturi quam laborum ipsam, facilis iure tempore dicta repellat obcaecati quaerat. Fugit, quas recusandae?
-            </p>
-        </section>
        
 
     </>);
